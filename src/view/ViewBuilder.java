3b96c40661f3953155d159decbbf1fe5dac09a1a
Wessel <wesselvanz@gmail.com>
2019-02-04 15:04:13 +0100

    change in code (not finished)

--- src/view/ViewBuilder.java
+++ src/view/ViewBuilder.java
@@ -1,17 +1,19 @@
 package view;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+
+
 public class ViewBuilder extends Application {
+
+    private static final int STARTING_HOUR = 6;
+    private static final int ENDING_HOUR = 24;
 
     private GridPane gridPane = new GridPane();
     private Canvas canvas = new Canvas();
@@ -31,13 +33,20 @@ public class ViewBuilder extends Application {
         buildVBox();
         buildHBox();
 
+        style();
+
+        draw();
+
         GridPane.setConstraints(this.canvas, 1, 1);
         GridPane.setConstraints(this.vBox, 0, 1);
         GridPane.setConstraints(this.hBox, 1, 0);
 
         this.gridPane.getChildren().addAll(this.canvas, this.vBox, this.hBox);
 
-        Scene scene = new Scene(this.gridPane);
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(this.gridPane);
+
+        Scene scene = new Scene(scrollPane, this.gridPane.getWidth(), 500);
 
         primaryStage.setTitle("School Agenda Manager");
         primaryStage.setScene(scene);
@@ -46,10 +55,7 @@ public class ViewBuilder extends Application {
 
     private void buildVBox() {
 
-        this.vBox.setSpacing(2);
-        this.vBox.setMaxHeight(500);
-
-        for (double i = 0; i < 24; i += 0.5) {
+        for (double i = STARTING_HOUR; i < ENDING_HOUR; i += 0.5) {
 
             double modulo = i % 1;
             int hour = (int)(i - modulo);
@@ -63,23 +69,62 @@ public class ViewBuilder extends Application {
                 string += hour + ":00";
             }
 
-            this.vBox.getChildren().add(new Label(string));
+            Label label = new Label(" " + string);
+            label.setStyle(
+                "-fx-pref-width: 100;" +
+                "-fx-border-style: solid inside;" +
+                "-fx-border-width: 2 0 0 0;" +
+                "-fx-border-color: lightgrey;"
+            );
+
+            this.vBox.getChildren().add(label);
         }
     }
 
     private void buildHBox()
     {
 
-        String lokalen[] = new String[] {"Lokaal A", "Lokaal B", "Lokaal C", "Lokaal D", "Lokaal E"};
-
-        this.hBox.setSpacing(10);
+        String lokalen[] = new String[] {"Lokaal A", "Lokaal B", "Lokaal C", "Lokaal D", "Lokaal E", "Lokaal F", "Lokaal G"};
 
         for (String lokaal : lokalen) {
 
-            Label label = new Label(lokaal);
-            label.minWidth(200);
+            Label label = new Label(" " + lokaal);
+            label.setStyle(
+                "-fx-pref-width: 100;" +
+                "-fx-border-style: solid inside;" +
+                "-fx-border-width: 0 0 0 2;" +
+                "-fx-border-color: lightgrey;"
+            );
 
             hBox.getChildren().add(label);
         }
+    }
+
+    private void draw()
+    {
+
+    }
+
+    private void style()
+    {
+
+        this.gridPane.setStyle(
+            "-fx-border-style: solid inside;" +
+            "-fx-border-color: lightgrey;" +
+            "-fx-border-insets: 2;" +
+            "-fx-border-width: 2;"
+        );
+
+        this.vBox.setStyle(
+            "-fx-border-style: solid inside;" +
+            "-fx-border-color: lightgrey;" +
+            "-fx-border-width: 0 2 0 0;"
+        );
+
+        this.hBox.setStyle(
+            "-fx-border-style: solid inside;" +
+            "-fx-border-color: lightgrey;" +
+            "-fx-border-width: 0 0 2 0;"
+        );
     }
 }
